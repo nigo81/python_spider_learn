34cb424b8fa89b83d8e8c2b886885ac0bf550a4c
nigo81 <tujiabing81@163.com>
2019-07-11 17:29:38 +0800

    正式版1.0

--- finance report/cbs_download.py
+++ finance report/cbs_download.py
@@ -14,26 +14,7 @@ class Login(object):
         self.signin_url='https://caibaoshuo.com/ajax_signin_or_signup'
         self.code_url='https://caibaoshuo.com/verification_codes'
         self.signed_url='https://caibaoshuo.com/account/level'
-        self.profit=[]
-        self.profit_data=[]
-        self.net_profit=[]
-        self.net_profit_data=[]
-        self.turnover=[]
-        self.turnover_data=[]
-        self.inventory=[]
-        self.inventory_data=[]
-        self.receivable=[]
-        self.receivable_data=[]
-        self.level=[]
-        self.level_data=[]
-        self.quick=[]
-        self.quick_data=[]
-        self.interest=[]
-        self.interest_data=[]
-        self.cash_ratio=[]
-        self.cash_ratio_data=[]
-        self.cash=[]
-        self.cash_data=[]
+        
     def login(self,phone):
         data={
             'utf8':'✓',
@@ -62,68 +43,37 @@ class Login(object):
     #     print(username)
     def statement(self,stock):
         page_headers={
-            'Referer':'https://caibaoshuo.com/companies/' + stock ,
+            'Referer':'https://caibaoshuo.com/companies/' + stock[0:6] ,
             'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36'
         }
-        url="https://caibaoshuo.com/companies/" + stock + "/financial_reports/ttm"
+        url="https://caibaoshuo.com/companies/" + stock[:6] + "/financial_reports/ttm"
         response=self.session.get(url,headers=page_headers)
         selector=etree.HTML(response.text)
         lists=selector.xpath('//ul[@class="summary-list"]/li/text()')
+        lists=lists[:10]
+        data=[stock]
+        i=0
         for li in lists:
-            print(li)
-        self.profit.append(lists[0].split("：",1)[0].split(" ",1)[1])
-        self.profit_data.append(lists[0].split("：",1)[1])
-        self.net_profit.append(lists[1].split("：",1)[0].split(" ",1)[1])
-        self.net_profit_data.append(lists[1].split("：",1)[1])
-        self.turnover.append(lists[2].split("：",1)[0].split(" ",1)[1])
-        self.turnover_data.append(lists[2].split("：",1)[1])
-        self.inventory.append(lists[3].split("：",1)[0].split(" ",1)[1])
-        self.inventory_data.append(lists[3].split("：",1)[1])
-        self.receivable.append(lists[4].split("：",1)[0].split(" ",1)[1])
-        self.receivable_data.append(lists[4].split("：",1)[1])
-        self.level.append(lists[5].split("：",1)[0].split(" ",1)[1])
-        self.level_data.append(lists[5].split("：",1)[1])
-        self.quick.append(lists[6].split("：",1)[0].split(" ",1)[1])
-        self.quick_data.append(lists[6].split("：",1)[1])
-        self.interest.append(lists[7].split("：",1)[0].split(" ",1)[1])
-        self.interest_data.append(lists[7].split("：",1)[1])
-        self.cash_ratio.append(lists[8].split("：",1)[0].split(" ",1)[1])
-        self.cash_ratio_data.append(lists[8].split("：",1)[1])
-        self.cash.append(lists[9].split("：",1)[0].split(" ",1)[1])
-        self.cash_data.append(lists[9].split("：",1)[1])
-
-
+            i=i+1
+            data.append(li.split("：",1)[0].split(" ",1)[1])
+            data.append(li.split("：",1)[1])
+        return data
 if __name__ == "__main__":
     login = Login()
     login.login('18080072071')
     
     df=pd.read_csv('./data/company.csv',encoding='gb2312')
     stock_codes=list(df.ts_code)
-
+    stock_codes=stock_codes[:10]
+    
+    count=0
+    
     for code in stock_codes:
-        login.statement(code[0:6])
-    out={
-        "stock_codes":stock_codes,
-        "profit":login.profit,
-        "profit_data":login.profit_data,
-        "net_profit":login.net_profit,
-        "net_profit_data":login.net_profit_data,
-        "turnover":login.net_profit,
-        "turnover_data":login.net_profit_data,
-        "inventory":login.inventory,
-        "inventory_data":login.inventory_data,
-        "receivable":login.receivable,
-        "receivable_data":login.receivable_data,
-        "level":login.level,
-        "level_data":login.level_data,
-        "quick":login.quick,
-        "quick_data":login.quick_data,
-        "interet":login.interest,
-        "interet_data":login.interest_data,
-        "cash_ratio":login.cash_ratio,
-        "cash_ratio_data":login.cash_ratio_data,
-        "cash":login.cash,
-        "cash_data":login.cash_data
-    }
-    df=pd.DataFrame(out)
-    df.to_csv('./data/statement.csv',mode='a',encoding='gb2312',index=False,header=False)
+        out=[]
+        result=[]
+        result=login.statement(code)
+        out.append(result)
+        df=pd.DataFrame(out)
+        df.to_csv('./data/statement.csv',mode='a',encoding='gb2312',index=False,header=False)
+        count=count+1
+        print("进度：" + str(count) + "/" + str(len(stock_codes))+"    当前代码:" + code)
